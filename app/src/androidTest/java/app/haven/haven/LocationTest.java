e18da0fed4a224d81009725ea965cc65f33926a4
bdavis18 <32071401+bdavis18@users.noreply.github.com>
2018-04-09 19:12:14 -0400

    Changed line 40 on RegisterActivity from setContentView(R.layout.actvity_main) to setContentView(R.layout.actvity_side_bar)

---
+++ app/src/androidTest/java/app/haven/haven/LocationTest.java
@@ -0,0 +1,83 @@
+package app.haven.haven;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import app.haven.haven.Model.shelters.Location;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by bdavis18 on 4/9/2018.
+ * JUnit tests for the distanceTo method in the Location class
+ * M10
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class LocationTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thatLatLowTest() {
+        Location dis = new Location(0, 0);
+        Location that = new Location(-95, 0);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thatLongLowTest() {
+        Location dis = new Location(0, 0);
+        Location that = new Location(0, -100);
+        dis.distanceTo(that);
+    }
+
+    public void thisLatLowTest() {
+        Location that = new Location(0, 0);
+        Location dis = new Location(-95, 0);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thisLongLowTest() {
+        Location that = new Location(0, 0);
+        Location dis = new Location(0, -100);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thatLatHighTest() {
+        Location dis = new Location(0, 0);
+        Location that = new Location(95, 0);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thatLongHighTest() {
+        Location dis = new Location(0, 0);
+        Location that = new Location(0, 100);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thisLatHighTest() {
+        Location that = new Location(0, 0);
+        Location dis = new Location(95, 0);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void thisLongHighTest() {
+        Location that = new Location(0, 0);
+        Location dis = new Location(0, 100);
+        dis.distanceTo(that);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void zero() {
+        Location dis = new Location(0, 0);
+        Location that = new Location(0, 0);
+        assertEquals(0, dis.distanceTo(that));
+    }
+}
+
--- app/src/main/java/app/haven/haven/Model/shelters/Location.java
+++ app/src/main/java/app/haven/haven/Model/shelters/Location.java
@@ -6,18 +6,39 @@ import com.google.android.gms.maps.model.LatLng;
  *  Taken from some Princeton class, it's open source and will be useful
  ******************************************************************************/
 
-class Location {
+public class Location {
     private double longitude;
     private double latitude;
 
+    /**
+     * Constructor method for Location class
+     * @param latitude the latitude of the shelter
+     * @param longitude the longitude of the shelter
+     */
     public Location(double latitude, double longitude) {
         this.latitude  = latitude;
         this.longitude = longitude;
     }
 
-    // return distance between this location and that location
-    // measured in statute miles
+    /**
+     * Calculates the location between this location and that location (in statute miles)
+     * @param that that location
+     * @return  distance between this location and that location
+     * measured in statute miles
+     */
     public double distanceTo(Location that) {
+        if (this.latitude < -90 || that.latitude < -90) {
+            throw new IllegalArgumentException("latitude can't be less than -90");
+        }
+        if (this.latitude > 90 || that.latitude > 90) {
+            throw new IllegalArgumentException("latitude can't exceed 90");
+        }
+        if (this.longitude < -180 || that.longitude < -180) {
+            throw new IllegalArgumentException("longitude can't be less than -180");
+        }
+        if (this.longitude > 180 || that.longitude > 180) {
+            throw new IllegalArgumentException("longitude can't exceed 180");
+        }
         double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
         double lat1 = Math.toRadians(this.latitude);
         double lon1 = Math.toRadians(this.longitude);
@@ -30,16 +51,29 @@ class Location {
 
         // each degree on a great circle of Earth is 60 nautical miles
         double nauticalMiles = 60 * Math.toDegrees(angle);
-        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-        return statuteMiles;
+//        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
+//        return statuteMiles;
+        return STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
     }
 
+    /**
+     *
+     * @return a new LatLng object based on the class's latitude and longitude instance variables
+     */
     public LatLng toLatLng() {
         return new LatLng(latitude, longitude);
     }
-    // return string representation of this point
+
+    /**
+     * the class's toString method
+     * @return string representation of this point
+     */
     public String toString() {
         return "(" + latitude + ", " + longitude + ")";
     }
+
+    private double getLongitude() {return longitude;}
+
+    private double getLatitude() {return latitude;}
 
 }
